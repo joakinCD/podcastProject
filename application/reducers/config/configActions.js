d634704a18bbd586cb7ecbb7b149507612011efc
joaquin-gopick <140534244+joakinCD@users.noreply.github.com>
2023-08-08 12:10:19 +0200

    funcionalidades cabecera

---
+++ application/reducers/config/configActions.js
@@ -0,0 +1,22 @@
+import { SET_NAVIGATION_REDUX,SET_LOADING } from "./configTypes";
+
+export const setNavigationRedux = (navigationRedux) => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_NAVIGATION_REDUX,
+      payload: {
+        navigationRedux: navigationRedux
+      }
+    });
+  };
+};
+export const setLoading = (loading) => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_LOADING,
+      payload: {
+        loading: loading
+      }
+    });
+  };
+};
